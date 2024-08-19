f0b720a3dc0132183b71b01e52d58ddc8b3aacc5
ToQuery <toquery@qq.com>
2024-08-19 20:04:56 +0800

    first commit

---
+++ internal/service/service.go
@@ -0,0 +1,29 @@
+package service
+
+import (
+	"example-nunu/internal/repository"
+	"example-nunu/pkg/jwt"
+	"example-nunu/pkg/log"
+	"example-nunu/pkg/sid"
+)
+
+type Service struct {
+	logger *log.Logger
+	sid    *sid.Sid
+	jwt    *jwt.JWT
+	tm     repository.Transaction
+}
+
+func NewService(
+	tm repository.Transaction,
+	logger *log.Logger,
+	sid *sid.Sid,
+	jwt *jwt.JWT,
+) *Service {
+	return &Service{
+		logger: logger,
+		sid:    sid,
+		jwt:    jwt,
+		tm:     tm,
+	}
+}
